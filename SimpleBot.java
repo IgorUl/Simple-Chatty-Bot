185edd461012d7ff990f617f3fe4dacb6de68c40
tcracked <alkoboy90@gmail.com>
2020-02-23 18:14:54 +0300

    Stage 2

--- SimpleBot.java
+++ SimpleBot.java
@@ -1,7 +1,31 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        final String botName = "Tcracked";
+        final int birthYear = 2020;
+        final String yourName;
+        int age;
+        int num;
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+        yourName = sc.nextLine();
+        System.out.println("What a great name you have, " + yourName + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        age = (sc.nextInt() * 70 + sc.nextInt() * 21 + sc.nextInt() * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        num = sc.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i + "!");
+        }
+        System.out.println("Completed, have a nice day!");
+
     }
 }
